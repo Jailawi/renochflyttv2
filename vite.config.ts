75680cf616883ac35473c906e85ec452f0ebaa6f
Jailawi <mustafa.al-jailawi@mail.com>
2025-11-29 12:14:24 +0100

    Add GTM and dataLayer

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/offertforfragan',
+  base: './offertforfragan',
   plugins: [VueRouter(), vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
