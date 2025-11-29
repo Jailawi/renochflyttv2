57a88e17465d6a1138d41c7b31bbb554b92f6a8d
Jailawi <mustafa.al-jailawi@mail.com>
2025-11-29 12:43:47 +0100

    update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/offertforfragan/'),
   routes,
 })
